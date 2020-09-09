110ce224dfb2287ef1fa6a75decf938231f7e4d5
Erlend Eriksen <erlend_ne@hotmail.com>
2020-09-09 15:16:50 +0200

    unfinished msg work

--- examples/msg_demo/prod.c
+++ examples/msg_demo/prod.c
@@ -3,13 +3,46 @@
 
 int main()
 {
-  msg_def(msg, int, a, 1, float, b, 1, char, c, 32);
-  msg.a[0] = 5;
-  msg.b[0] = 3.14;
-  sprintf(msg.c, "hei");
 
-  printf("%s: %d, %d, %f, %s\n", msg.id, msg.size, msg.a[0], msg.b[0], msg.c);
-  printf("%s%s", msg_print(msg), "\n");
+  char buffer0[512];
+
+  {
+    msg_def(msg0, int, a, 1, float, b, 1, char, c, 32);
+    msg0.a[0] = 5;
+    msg0.b[0] = 3.14;
+    sprintf(msg0.c, "hei");
+
+    printf("%s %d: %d, %f, %s\n", msg0.id, msg0.size, msg0.a[0], msg0.b[0], msg0.c);
+    //printf("%s%s", msg_print(msg0), "\n");
+
+    memcpy(buffer0, &msg0, sizeof(msg0));
+  }
+
+
+
+  {
+    msg_def(msg1, int, a, 1, char, c, 32, long, d, 1, long long, e, 1);
+    msg1.a[0] = 19;
+    sprintf(msg1.c, "hade");
+    msg1.d[0] = 628;
+    msg1.e[0] = 345345345;
+
+    printf("%s %d: %d, %s, %ld, %lld\n", msg1.id, msg1.size, msg1.a[0], msg1.c, msg1.d[0], msg1.e[0]);
+    //printf("%s%s", msg_print(msg1), "\n");
+
+    char buffer1[512];
+    memcpy(buffer1, buffer0, msg_header_size(msg1));
+
+    if (msg_match(msg, buffer1))
+    {
+      printf("no match\n");
+      return 1;
+    }
+
+    memcpy(buffer1 + msg_header_size(msg1), buffer0 + msg_header_size(msg1), msg_size(buffer) - msg_header_size(msg1));
+
+    msg_parse(msg1, buffer)
+  }
 
   return 0;
 }
--- include/msg.h
+++ include/msg.h
@@ -1,8 +1,7 @@
 #include <assert.h>
-#include <stdint.h>
+#include <string.h>
 #include "macro.h"
 
-#define NAME_SIZE 8
 #define ID_SIZE 8
 #define SIZE_TYPE int
 
@@ -10,7 +9,7 @@
 #define MSG_NO_EVAL_VERIFY(...) \
     IF ( IS_LIST_NOT_EMPTY( __VA_ARGS__ )  ) \
     ( \
-        static_assert(sizeof(STRINGIFY(HEAD(__VA_ARGS__))) < NAME_SIZE, "field name too large: sizeof(" STRINGIFY(HEAD1(__VA_ARGS__)) ") > " STRINGIFY(NAME_SIZE) ); \
+        static_assert(sizeof(STRINGIFY(HEAD1(__VA_ARGS__))) < ID_SIZE, "field id too large: sizeof(" STRINGIFY(HEAD1(__VA_ARGS__)) ") > " STRINGIFY(ID_SIZE) ); \
         DEFER2 ( MSG_INDIRECT_VERIFY ) () (TAIL2(__VA_ARGS__)) \
     )
 
@@ -18,7 +17,7 @@
 #define MSG_NO_EVAL_DECL(...)                                                                                     \
     IF ( IS_LIST_NOT_EMPTY( __VA_ARGS__ )  )                                                                      \
     (                                                                                                             \
-        char CAT(HEAD1(__VA_ARGS__), _name)[NAME_SIZE];                                                           \
+        char CAT(HEAD1(__VA_ARGS__), _id)[ID_SIZE];                                                               \
         SIZE_TYPE CAT(HEAD1(__VA_ARGS__), _size);                                                                 \
         HEAD(__VA_ARGS__) HEAD1(__VA_ARGS__) [HEAD2(__VA_ARGS__)];                                                \
                                                                                                                   \
@@ -29,8 +28,8 @@
 #define MSG_NO_EVAL_COMPOUND(...)                                                                                 \
     IF ( IS_LIST_NOT_EMPTY( __VA_ARGS__ )  )                                                                      \
     (                                                                                                             \
-        STRINGIFY(HEAD1(__VA_ARGS__)) COMMA                                    \
-        sizeof(HEAD(__VA_ARGS__)) * HEAD2(__VA_ARGS__) COMMA                   \
+        STRINGIFY(HEAD1(__VA_ARGS__)) COMMA                                                                       \
+        sizeof(HEAD(__VA_ARGS__)) * HEAD2(__VA_ARGS__) COMMA                                                      \
         {} COMMA                   \
         DEFER2 ( MSG_INDIRECT_COMPOUND ) () (TAIL2(__VA_ARGS__))                                                  \
     )
@@ -38,6 +37,7 @@
 #define MSG_DEF(_id, ...)                                                                                  \
     static_assert(sizeof(STRINGIFY(_id)) < ID_SIZE, "id too large: sizeof(" STRINGIFY(_id) ") > " STRINGIFY(ID_SIZE) ); \
     EVAL(MSG_NO_EVAL_VERIFY(__VA_ARGS__))                                                                               \
+    struct 
     struct __attribute__((__packed__)) { char id[ID_SIZE]; SIZE_TYPE size; EVAL(MSG_NO_EVAL_DECL(__VA_ARGS__)) }        \
         _id = { STRINGIFY(_id), sizeof(_id), EVAL(MSG_NO_EVAL_COMPOUND(__VA_ARGS__)) }
 
@@ -49,59 +49,102 @@ struct __attribute__((__packed__))
 {
   char id[ID_SIZE] = "msg";
   SIZE_TYPE size = 88;
-  char a_name[NAME_SIZE] = "a";
+  char a_id[ID_SIZE] = "a";
   SIZE_TYPE a_size = 4;
   int a[1];
-  char b_name[NAME_SIZE] = "b";
+  char b_id[ID_SIZE] = "b";
   SIZE_TYPE b_size = 4;
   float b[1];
-  char c_name[NAME_SIZE] = "c";
+  char c_id[ID_SIZE] = "c";
   SIZE_TYPE c_size = 32;
   float c[32];
 }
 msg =
 {
-  . a_name = "a" , . a_size = sizeof(int) ,
-  . b_name = "b" , . b_size = sizeof(float) ,
+  . a_id = "a" , . a_size = sizeof(int) ,
+  . b_id = "b" , . b_size = sizeof(float) ,
 };
 */
 #define msg_def MSG_DEF
 
+#define msg_match(msg, buffer) (strcmp((typeof(msg)*(buffer)->id), msg.id) == 0)
 
-// printf(msg_print(msg));
-#define msg_print(msg)                                    \
-  ({                                                      \
-    static char ret[sizeof(msg) * 10];                    \
-    sprintf(ret, "id: %s, size: %d", msg.id, msg.size);   \
-                                                          \
-    int i = sizeof(msg.id) + sizeof(msg.size);            \
-    while (i < (msg).size)                                \
-    {                                                     \
-      void *p;                                            \
-      p = (void*)&(msg) + i;                              \
-      char *c = (char*)p;                                 \
-                                                          \
-      /*for (;c[i] != '\0'; ++i) {}*/                     \
-      sprintf(ret, "%s, %s: ", ret, c);                   \
-                                                          \
-      i += NAME_SIZE;                                     \
-      p = (void*)&(msg) + i;                              \
-      SIZE_TYPE field_size = *(SIZE_TYPE*)p;              \
-                                                          \
-      i += sizeof(field_size);                            \
-      p = (void*)&(msg) + i;                              \
-                                                          \
-      switch(field_size)                                  \
-      {                                                   \
-        case 4:                                           \
-          sprintf(ret, "%s, %f", ret, *(float*)p);        \
-          break;                                          \
-        default:                                          \
-          sprintf(ret, "%s, %s", ret, (char*)p);          \
-          break;                                          \
-      }                                                   \
-                                                          \
-      i += field_size;                                    \
-    }                                                     \
-    ret;                                                  \
-  })
+#define msg_header_size(msg) (sizeof(msg.id) + sizeof(msg.size))
+
+#define msg_size(buffer) (typeof(msg)*(buffer)->msg.size)
+
+// msg_parse(msg, buffer);
+#define msg_parse(msg, buffer)                          \
+do {                                                    \
+  int i = msg_header_size(msg);                         \
+  while (i < (msg).size)                                \
+  {                                                     \
+    void *p;                                            \
+    p = (void*)&(msg) + i;                              \
+    {                                                   \
+      char *field_id = (char*)p;                        \
+      /*for (;c[i] != '\0'; ++i) {}*/                   \
+
+      
+    }                                                   \
+                                                        \
+    i += sizeof(msg.id);                                \
+    p = (void*)&(msg) + i;                              \
+    SIZE_TYPE field_size = *(SIZE_TYPE*)p;              \
+                                                        \
+    i += sizeof(field_size);                            \
+    p = (void*)&(msg) + i;                              \
+                                                        \
+    switch(field_size)                                  \
+    {                                                   \
+      case 4:                                           \
+        sprintf(ret, "%s, %f", ret, *(float*)p);        \
+        break;                                          \
+      default:                                          \
+        sprintf(ret, "%s, %s", ret, (char*)p);          \
+        break;                                          \
+    }                                                   \
+                                                        \
+    i += field_size;                                    \
+  }                                                     \
+  ret;                                                  \
+} while(0)
+
+
+//// printf(msg_print(msg));
+//#define msg_print(msg)                                    \
+//  ({                                                      \
+//    static char ret[sizeof(msg) * 10];                    \
+//    sprintf(ret, "id: %s, size: %d", msg.id, msg.size);   \
+//                                                          \
+//    int i = sizeof(msg.id) + sizeof(msg.size);            \
+//    while (i < (msg).size)                                \
+//    {                                                     \
+//      void *p;                                            \
+//      p = (void*)&(msg) + i;                              \
+//      char *c = (char*)p;                                 \
+//                                                          \
+//      /*for (;c[i] != '\0'; ++i) {}*/                     \
+//      sprintf(ret, "%s, %s: ", ret, c);                   \
+//                                                          \
+//      i += ID_SIZE;                                     \
+//      p = (void*)&(msg) + i;                              \
+//      SIZE_TYPE field_size = *(SIZE_TYPE*)p;              \
+//                                                          \
+//      i += sizeof(field_size);                            \
+//      p = (void*)&(msg) + i;                              \
+//                                                          \
+//      switch(field_size)                                  \
+//      {                                                   \
+//        case 4:                                           \
+//          sprintf(ret, "%s, %f", ret, *(float*)p);        \
+//          break;                                          \
+//        default:                                          \
+//          sprintf(ret, "%s, %s", ret, (char*)p);          \
+//          break;                                          \
+//      }                                                   \
+//                                                          \
+//      i += field_size;                                    \
+//    }                                                     \
+//    ret;                                                  \
+//  })
