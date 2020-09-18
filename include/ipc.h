675f5777ab3eca28ab818790213e246f80a352aa
Erlend Eriksen <erlend_ne@hotmail.com>
2020-09-18 19:06:27 +0200

    ipc: use seq_lock instead of calm_seq_lock

--- include/ipc.h
+++ include/ipc.h
@@ -1,14 +1,23 @@
 #include "shmem.h"
+#include "seq_lock.h"
 #include "calm_seq_lock.h"
 
 
 
 #ifdef __cplusplus
 
-template<typename STORAGE>
-using ipc_sender = shmem_data<calm_seq_lock<STORAGE, 64>, true>;
+//template<typename STORAGE>
+//using ipc_sender = shmem_data<calm_seq_lock<STORAGE, 64>, true>;
+//
+//template<typename STORAGE>
+//using ipc_receiver = shmem_data<calm_seq_lock<STORAGE, 64>, false>;
+
+
 
 template<typename STORAGE>
-using ipc_receiver = shmem_data<calm_seq_lock<STORAGE, 64>, false>;
+using ipc_sender = shmem_data<seq_lock<STORAGE>, true>;
+
+template<typename STORAGE>
+using ipc_receiver = shmem_data<seq_lock<STORAGE>, false>;
 
 #endif
