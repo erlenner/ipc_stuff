d49f6a86ca79857c2b81d56c0c73030da73c88c5
Erlend Eriksen <erlend_ne@hotmail.com>
2020-09-21 12:33:03 +0200

    proper assert

--- include/ipc/debug.h
+++ include/ipc/debug.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <ipc/macro.h>
 
 #define error(...) fprintf(stderr, __VA_ARGS__);
 
@@ -8,7 +9,7 @@
 
 //#define debug_assert(cond, ...) { if (!(cond)) { error("[%s\tline %5d] ", __FILE__, __LINE__); error(__VA_ARGS__); error(" [aborting]\nSTDERR: %s\n", strerror(errno)); exit(EXIT_FAILURE); } }
 
-#define debug_assert(cond, ...) { if (!(cond)) { error("[%s\tline %5d] ", __FILE__, __LINE__); error("%s\n", strerror(errno)); __VA_ARGS__; } }
+#define debug_assert(cond, ...) { if (!(cond)) { error("[%s\tline %5d] assertion failed: (" STRINGIFY(cond) ") errno: ", __FILE__, __LINE__); error("%s\n", strerror(errno)); __VA_ARGS__; } }
 
 #define debug_assert_v(cond, ...) { if (!(cond)) { error("[%s\tline %5d] ", __FILE__, __LINE__); error(__VA_ARGS__); error("%s\n", strerror(errno)); } }
 
