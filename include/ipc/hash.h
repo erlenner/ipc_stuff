e5fdf6c2d2e39ddfb9015a9b803b820053d8135e
Erlend Eriksen <erlend_ne@hotmail.com>
2020-10-06 15:59:08 +0200

    prettify

--- include/ipc/hash.h
+++ include/ipc/hash.h
@@ -1,47 +1,6 @@
 #pragma once
 #ifdef __cplusplus
 
-//// constexpr string
-//// https://github.com/arcolight/ecl/blob/master/include/ecl/str_const.hpp
-//class str_const
-//{
-//private:
-//  const char* const p_;
-//  const std::size_t sz_;
-//public:
-//  template<std::size_t N>
-//  constexpr str_const(const char (&a)[N]) :
-//    p_(a), sz_(N - 1)
-//  {}
-//  constexpr char operator[](std::size_t n)
-//  {
-//    return n < sz_ ? p_[n] : 0;
-//  }
-//  constexpr std::size_t size() {
-//    return sz_;
-//  } // size()
-//};
-
-
-//#define const_str(string_literal) 
-//
-//template <char c>
-//class char_t
-//{
-//  char character = c;
-//}
-//
-//template <typename... char_ts>
-//class string_t
-//{
-//  
-//}
-//
-//auto create_string_t(const char* s)
-//{
-//  return 
-//}
-
 
 #include <cstdint>
 
@@ -55,8 +14,5 @@ constexpr std::uint32_t operator"" _hash(char const* s, std::size_t count)
 {
     return hash_fnv1a_32(s, count);
 }
-
-//constexpr std::uint32_t check = "0123456789ABCDEF"_hash;
-
 
 #endif
