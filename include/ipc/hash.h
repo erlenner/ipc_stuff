28844c6c7598d9ef754a7e3f4589805b71fad78f
Erlend Eriksen <erlend_ne@hotmail.com>
2020-10-07 14:19:56 +0200

    prettify

--- include/ipc/hash.h
+++ include/ipc/hash.h
@@ -1,18 +1,17 @@
 #pragma once
 #ifdef __cplusplus
 
-
-#include <cstdint>
+#include "stdint.h"
 
 // FNV-1a 32bit hashing algorithm.
-constexpr std::uint32_t hash_fnv1a_32(char const* s, std::size_t count)
+constexpr uint32_t hash_fnv1a_32(const char* s, size_t count)
 {
-    return ((count ? hash_fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u;
+  return ((count ? hash_fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u;
 }
 
-constexpr std::uint32_t operator"" _hash(char const* s, std::size_t count)
+constexpr uint32_t operator ""_hash(const char* s, size_t count)
 {
-    return hash_fnv1a_32(s, count);
+  return hash_fnv1a_32(s, count);
 }
 
 #endif
